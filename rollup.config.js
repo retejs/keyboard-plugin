70af4adc66a19d47e203398aaf55edaaa5ffab5a
Vitaliy Stoliarov <huccah@gmail.com>
2018-05-31 02:06:08 +0300

    initial

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+export default {
+    input: 'index.js',
+    output: {
+        file: 'build/index.js',
+        name: 'KeyboardPlugin',
+        format: 'umd',
+        sourcemap: true
+    },
+    plugins: []
+}
