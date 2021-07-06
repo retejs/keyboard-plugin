e044083465d5bd9dbe83970f9c93d11df573a982
Hugo Sales <hugo@hsal.es>
2021-07-06 22:46:07 +0000

    Switch from event.code to event.key, to allow for remapped layouts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 function install(editor) {
     editor.view.container.focus();
-    
     editor.on('keydown', e => {
-        switch (e.code) {
-        case 'Delete':
+
+        switch (e.key) {
+        case 'Delete': // Delete a node
             editor.selected.each(n => editor.removeNode(n));
             break;
-        case 'Space':
+        case ' ': // Spawn a context menu at the mouse position, but make sure it's inside the editor
             let rect = editor.view.container.getBoundingClientRect();
             let event = new MouseEvent('contextmenu', {
                 clientX: rect.left + rect.width / 2,
