379b90eb9cc22dbf887ba338e431c1d453cc53cb
Hugo Sales <hugo@hsal.es>
2021-07-06 22:47:47 +0000

    Spawn context menu at mouse position, clamped to viewport

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 function install(editor) {
     editor.view.container.focus();
-    
+
     editor.on('keydown', e => {
         switch (e.code) {
         case 'Delete':
             editor.selected.each(n => editor.removeNode(n));
             break;
-        case 'Space':
-            let rect = editor.view.container.getBoundingClientRect();
-            let event = new MouseEvent('contextmenu', {
-                clientX: rect.left + rect.width / 2,
-                clientY: rect.top + rect.height / 2
-            });
 
-            editor.trigger('contextmenu', { e: event, view: editor.view }); 
-            break;    
+        case 'Space': // Spawn a context menu at the mouse position, but make sure it's inside the editor
+            const mouse = editor.view.area.mouse;
+            const rect = editor.view.container.getBoundingClientRect();
+            const event = new MouseEvent('contextmenu', {
+                clientX: clamp(mouse.x, rect.left, rect.right),
+                clientY: clamp(mouse.y, rect.top, rect.bottom),
+            });
+            editor.trigger('contextmenu', { e: event, view: editor.view });
+            break;
+
         default: break;
         }
     });
